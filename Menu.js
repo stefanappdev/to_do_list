cd04dadc845647f091075139c40cbc73757792d8
Stefan Mitchell <stevenbrown2dn@gmail.com>
2022-03-22 16:37:17 -0500

    Added functionality to edit and delete buttons

--- Menu.js
+++ Menu.js
@@ -1,49 +1,65 @@
-function add_item() {
+function handle_item() {
 
 	//document.createElement(<"ele">) to create a new element, document.createTextNode(<txt>) to create textnode to add to new element, 
 	//then use <parent>.appendChild(child) ;
 
+
+let menu=document.getElementById("Menu");
 let item=document.querySelector("#item_name");
-const new_item=document.createElement("li");
-const menu=document.getElementById("Menu");
+let menuitem=document.createElement("li");
+let par=document.createElement("p");
+let div=document.createElement("div");
+let del=document.createElement("button");
+let edit=document.createElement("button");
+del.textContent="delete";
+edit.textContent="edit";
+par.textContent=item.value;
+div.appendChild(par);
+div.appendChild(del);
+div.appendChild(edit);
 
-menu.appendChild(new_item);
-new_item.textContent=item.value;
 
-alert("New item added to menu");
+//handles delete functionality of itms
+del.addEventListener("click",()=>{
+menu.removeChild(menuitem);
+alert("item removed from menu");
+item.value="";
+		});
 
 
-item.value=" ";
-	} 
+//handle for edit button 
+edit.addEventListener("click",()=>{
 
+let editbar=document.createElement("input");
+editbar.setAttribute("type","text");
+let ok=document.createElement("button");
+ok.textContent="Ok";
+div.insertBefore(editbar,del);//inssert editbar before button
+div.insertBefore(ok,del);
+edit.setAttribute("style","visibility:hidden");
+del.setAttribute("style","visibility:hidden");
+
+//after clicking ok
+ok.addEventListener("click",()=>{
+par.textContent=editbar.value;
+ok.remove();
+editbar.remove();
+edit.setAttribute("style","visibility:visible");
+del.setAttribute("style","visibility:visible");
+alert(" An item has been updated");
+		})
+		
+
+		});
+
+
+
+menuitem.appendChild(div);
+
+menu.appendChild(menuitem);
+alert("Item added to list");
+item.value="";
 	
-
-
-
-function delete_item() {
-
-//<ele>.remove(),<parentelement>.removeChild(<child>) are methods to delete html elements in js
-
-let item=document.getElementById("item_name");
-let li_list=document.querySelectorAll("#Menu>li");
-const menu=document.getElementById("Menu");
-
-for (let x in li_list){
-	
-	if (item.value===li_list[x].textContent){
-
-		menu.removeChild(li_list[x]);
-		alert("Item removed from list");
-		item.value=" ";
-
-		}
-
-
-
-		}
-
-
-
 } 
 
 
